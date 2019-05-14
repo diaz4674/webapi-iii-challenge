1b71970d1f6fe64095163a95c6c9b23d2136e93e
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-05-13 22:56:57 -0700

    working logger

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const logger = require('morgan')
 
 const server = express();
 
+//Global Middleware
+
+//built in middleware
+server.use(express.json());
+
+//third party middleware
+server.use(helmet());
+server.use(logger('dev'));
+
+//custom middleware 
+server.use(infoLogger);
+
+//router
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
 
 //custom middleware
 
-function logger(req, res, next) {
-
+function infoLogger(req, res, next) {
+  console.log(`${req.method} Request, Currently on URL: "${req.url}", Timestamp: ${Date()} `);
+  next();
 };
 
 module.exports = server;
