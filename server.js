91c9f863466293bea45707e1882151c7a7b9857f
Miguel Diaz <37884809+diaz4674@users.noreply.github.com>
2019-05-13 23:27:39 -0700

    GET working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan')
 
+const usersRouter = require('./users/userRouter.js');
+
 const server = express();
 
 //Global Middleware
@@ -17,6 +19,8 @@ server.use(logger('dev'));
 server.use(infoLogger);
 
 //router
+server.use('/api/users', usersRouter);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
@@ -27,5 +31,13 @@ function infoLogger(req, res, next) {
   console.log(`${req.method} Request, Currently on URL: "${req.url}", Timestamp: ${Date()} `);
   next();
 };
+
+//Error handler 
+server.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Error',
+    err
+  })
+})
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,6 +1,9 @@
-const express = 'express';
+const express = require('express');
 
+const Users = require('./userDb.js')
 const router = express.Router();
+
+
 
 router.post('/', (req, res) => {
 
@@ -10,12 +13,19 @@ router.post('/:id/posts', (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
-
+//GET router
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.get(req.query);
+        res.status(200).json(users);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({message: 'Error retriving the Users'})
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -32,8 +42,20 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+    try{
+        const {id} = req.params;
+        const user = await Users.findById(id)
 
+        if(user){
+            req.user = user;
+            next();
+        } else {
+            next({message: 'User not found; invalid id'})
+        }
+    } catch (err) {
+        res.status(500).json({message: 'Failed to process request'})
+    }
 };
 
 function validateUser(req, res, next) {
